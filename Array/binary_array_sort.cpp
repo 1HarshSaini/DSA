569af419671426ed7bbf8f83f73bcb3bdfe01164
Harsh Saini <103092274+1HarshSaini@users.noreply.github.com>
2022-04-13 23:05:48 +0530

    Add files via upload

---
+++ Array/binary_array_sort.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+    
+    
+    void binSort(int A[], int N)
+    {
+    //   int j=0;
+    //   for(int i=0;i<N;i++){
+    //         if(A[i]==0){
+    //           int temp=A[i];
+    //           A[i]=A[j];
+    //           A[j]=temp;
+    //           j++;
+    //       }
+    //   }
+    
+    // int i=0;  // this is baed on boare partition  of merge sorrt
+    // int j=N-1;
+    //     while(i<j){
+    //       while(A[i]==0){
+    //           i++;
+    //       }
+    //       while(A[j]==1){
+    //           j--;
+    //       }
+    //       if(i<j){
+    //           swap(A[i],A[j]);
+    //       }
+                
+    //     }
+    // }
